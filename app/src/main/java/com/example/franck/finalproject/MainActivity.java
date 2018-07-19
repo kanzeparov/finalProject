f8fcc4b12d3886e12b9b4be3925f4ddc2e7eea9d
Kanzeparov <kanzeparov@yandex.ru>
2018-07-19 16:30:06 +0300

    layout

--- app/src/main/java/com/example/franck/finalproject/MainActivity.java
+++ app/src/main/java/com/example/franck/finalproject/MainActivity.java
@@ -3,7 +3,39 @@ package com.example.franck.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/*
+Выпускной проект
+ Класс наследник
+  Интерфейс
+   Инкапсуляция
+    Структура массив
+    Примитивы
+    Преобразование типа
+     If,switch
+     Цикл
+
+     Возведение в степень через рекурсию
+     Возведение в степень через цикл
+     Конструкторы
+     Обработка исключения
+     Автосчетчик онлайн через треды
+     Абстрактный класс
+     Abstract
+     Class
+     Operation A, b, equals
+     Plus oper
+     MinusOper
+     multoper
+      Divoper
+      Zeroarithm
+      SchetThrwasOper
+      PowOper two meth recursion, cicle
+ShiftOper Exception not chislo Not divide Debugt
+ */
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
